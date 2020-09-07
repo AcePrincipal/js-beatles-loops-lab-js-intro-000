ff8d932854459be7a966e86f659dcffaae7a2527
Alexander Petrov <acepetrov96@gmail.com>
2020-09-07 15:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+var arr = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for(var i = 0; i < musicians.length; i++)
+  {
+    arr[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+
+  return arr;
+}
+function johnLennonFacts(arr) {
+  var i = (arr.length-1);
+  var arr1 = [];
+
+  while(i >= 0) {
+    arr1[i] = `${arr[i]}!!!`
+  }
+
+  return arr1;
+}
